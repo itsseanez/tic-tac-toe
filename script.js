259ebaa35b3a955086fca0a245c7f478d9693547
Sean Ezenwa <seanezenwa@gmail.com>
2023-02-08 17:46:13 -0500

    Change player selection based on chosen x or o

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 const gameBoard = (() => {
     // Creates array of tic tac toe spots
-    let _board= new Array(9);
+    let board= new Array(9);
     // displays board contents
     const displayBoard = () => {
-        for (let i=0; i < _board.length; i++) {
+        for (let i=0; i < board.length; i++) {
             const boardButton= document.createElement('button');
             const board= document.querySelector('#game-board');
-            boardButton.classList.add('board-button');
-            board.appendChild(boardButton)
+            boardButton.classList.add('board-button', 'regular-text');
+            board.appendChild(boardButton);
         };
     }
+
+    const boardDisplay= document.querySelectorAll('.board-button');
+    const xButton= document.getElementById('x');
+    const oButton= document.getElementById('o');
+    let playerChoice;
+    xButton.addEventListener('click', function() {
+        if(playerChoice= 'O') {
+            oButton.classList.remove('clicked');
+        }
+        playerChoice= 'X';
+        xButton.classList.remove('button');
+        xButton.classList.add('main-text', 'clicked');
+        oButton.classList.add('button', 'main-text');
+    });
+
+    oButton.addEventListener('click', function() {
+        if(playerChoice= 'X') {
+            xButton.classList.remove('clicked');
+        }
+        playerChoice= 'O';
+        oButton.classList.remove('button');
+        oButton.classList.add('main-text', 'clicked');
+        xButton.classList.add('button', 'main-text');
+    });
+    boardDisplay.forEach(function(boardButton)  {
+        boardButton.addEventListener('click', function() {
+            console.log(playerChoice);
+            boardButton.textContent= playerChoice;
+        });
+    });
     return {displayBoard};
 })();
+
+const game = (() => {
+    
+    
+    return {};
+})(); 
+
 gameBoard.displayBoard();
+
