0f38901197ded6e2d74c7a1fdd30531417d36428
Sean Ezenwa <seanezenwa@gmail.com>
2023-02-09 00:21:09 -0500

    Make board a 3x3 array

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const gameBoard = (() => {
     // Creates array of tic tac toe spots
-    let board= new Array(9);
+    let board= new Array(3);
+    for(let i=0; i < 3; i++) {
+        board[i]= new Array(3);
+    }
+    
     // displays board contents
     const displayBoard = () => {
-        for (let i=0; i < board.length; i++) {
-            const boardButton= document.createElement('button');
-            const board= document.querySelector('#game-board');
-            boardButton.classList.add('board-button', 'regular-text');
-            board.appendChild(boardButton);
-            boardButton.addEventListener('click', function() {
-                console.log(playerChoice);
-                boardButton.textContent= playerChoice;
-            });
-        };
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const boardButton= document.createElement('button');
+                const board= document.querySelector('#game-board');
+                boardButton.classList.add('board-button', 'regular-text');
+                board.appendChild(boardButton);
+
+                //Adds player choice to button
+                boardButton.addEventListener('click', function() {
+                    if(boardButton.textContent === '') {
+                        boardButton.textContent= playerChoice;
+                    }
+                });
+            }
+        }
     }
 
+    //Decide player choice
     const xButton= document.getElementById('x');
     const oButton= document.getElementById('o');
     let playerChoice;
+
     xButton.addEventListener('click', function() {
-        if(playerChoice= 'O') {
+        if(playerChoice=== 'O') {
             oButton.classList.remove('clicked');
         }
         playerChoice= 'X';
@@ -29,7 +40,7 @@ const gameBoard = (() => {
     });
 
     oButton.addEventListener('click', function() {
-        if(playerChoice= 'X') {
+        if(playerChoice=== 'X') {
             xButton.classList.remove('clicked');
         }
         playerChoice= 'O';
@@ -37,6 +48,7 @@ const gameBoard = (() => {
         oButton.classList.add('main-text', 'clicked');
         xButton.classList.add('button', 'main-text');
     });
+
     return {displayBoard};
 })();
 
